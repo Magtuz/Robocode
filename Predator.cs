04baab25559d8e1e8c8d200adcad60a8796a71bc
Magtuz <tuz_kaval77@tut.by>
2012-02-23 15:35:17 +0300

    first commit

---
+++ Predator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Robocode;
+
+namespace EMK
+{
+    public class Predator: Robot
+    {
+        public override void Run()
+        {
+            TurnLeft(Heading - 90);
+            TurnGunRight(90);
+            while (true)
+            {
+                Ahead(5000);
+                TurnRight(90);
+            }
+        }
+
+        public override void OnScannedRobot(ScannedRobotEvent e)
+        {
+            Fire(1);
+        }
+    }
+}
