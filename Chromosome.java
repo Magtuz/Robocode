f54c8fbb7abd60b5902b07cb06041f28f0b5659d
Magtuz <tuz_kaval77@tut.by>
2012-05-26 01:02:40 +0300

    'predator_0.8'

--- Chromosome.java
+++ Chromosome.java
@@ -5,7 +5,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+//import java.util.Collections;
 import java.util.Random;
 import java.util.ArrayList;
 
@@ -73,21 +75,21 @@ public void doubleCrossover(  Chromosome chromosome  ){
 	}
 	
 										
-	static void generate_battle() 		//создаем файл my.battle, в который записываем параметры боя	
+	static void generate_battle() 		//создаем файл battle, в который записываем параметры боя	
 	{
 		
-		
+		BufferedWriter out;
 		try 
-	    {
-	        	    
-	    BufferedWriter out = new BufferedWriter(new FileWriter("chromos.bs"));
-		
+	    {  	    
+		out = new BufferedWriter(new FileWriter("C:\\robocode\\battles\\bat.battle"));
 		out.write("robocode.battleField.width=800\n");
 		out.write("robocode.battleField.height=600\n");
-		out.write("robocode.battle.numRounds=20\n");
+		out.write("robocode.battle.numRounds=5\n");
 		out.write("robocode.battle.gunCoolingRate=0.1\n");
 		out.write("robocode.battle.rules.inactivityTime=450\n");																			
-		out.write("robocode.battle.selectedRobots=EMK.Predtor,sample.Wall\n");
+		out.write("robocode.battle.selectedRobots=EMK.Predator*,voidious.Diamond\n");
+		System.out.println("Записали battle\n");
+		out.close();
 	    }
 	    catch (IOException e)    { System.out.println("файл battle.txt не открывается\n");  }
 	} 
@@ -97,19 +99,26 @@ public void doubleCrossover(  Chromosome chromosome  ){
 			
 	
 	
+	public static int generation=0;
 	
+	////////////////////////////////////////////////////////////////////////////////////////
 	
-	/////////////////////////////////////////////////////
-	
-	public static int main(String[] args) 
+	public static void main(String[] args) 
 	{
 	
-	int x1 = 0, y1 = 0, x2 = 0, y2 = 0,generation=0,max_fitness=0;
+	//int generation=0;
+	int max_fitness=0;
 	Specimen specimen,best;
+	specimen= new Specimen();
+	best= new Specimen();
 	Helper.Init();					//создание нулевого поколения
+	System.out.println("ПОКОЛЕНИЕ инициаллизировано ");
 	Helper.selection();			//турнир
+	System.out.println("Селекция ");
+	System.out.println("Размер "+ Helper.Hlist.size());
 	Helper.destroy();				//разбока
-	Helper.crossandmut();			//селекция и мутация
+	System.out.println("Разрушено ");
+	Helper.crossandmut();			//скрещивание и мутация
 			
 			
 	generation++;
@@ -122,45 +131,47 @@ public void doubleCrossover(  Chromosome chromosome  ){
 			for(int j=0;j<Helper.POPULATION_COUNT;j++)
 				for(int k=0;k<Helper.POPULATION_COUNT;k++)
 					for(int z=0;z<Helper.POPULATION_COUNT;z++){
-						specimen.setIndiv(0,Helper.getShotAngle()[i]);
-						specimen.setIndiv(1,Helper.getMoveAngle()[j]);
-						specimen.setIndiv(2,Helper.getShotEnergy()[k]);
-						specimen.setIndiv(3,Helper.getShotEnergy()[z]);
+						specimen.setIndiv(0,Helper.getShotAngle(i));
+						specimen.setIndiv(1,Helper.getMoveAngle(j));
+						specimen.setIndiv(2,Helper.getShotEnergy(k));
+						specimen.setIndiv(3,Helper.getShotEnergy(z));
 						specimen.writetofile();	
 						generate_battle();
-						Runtime.getRuntime().exec("cmd /c robocode.bat");	//запуск робокода для теста робота
+						
+								//Runtime.getRuntime().exec("cmd /c start c:/robocode/robo.bat");	//запуск робокода для теста робота
+								//Runtime.getRuntime().exec("cmd /c  c:/robocode/robo.bat");
+							runProgramAndWait();
+							
 						specimen.scan_result();								//узнаем значение приспособленности
-						Helper.Hlist.add(specimen); 
+						Helper.Hlist.add(specimen); 						//добавление особи в файл
 					}	
 		Helper.selection();			//турнир
 		max_fitness=Helper.maxfitness();//нужно узнать лучшую и среднию приспособленность на данный момент
 		best=Helper.find_max_fitness(Helper.Hlist);
 		Helper.destroy();				//разбока
 		Helper.crossandmut();
-		System.out.println("ПОКОЛЕНИЕ " + generation);
+		System.out.println("ПОКОЛЕНИЕ " + (++generation));
+		System.out.println("Лучшая приспособленность " + max_fitness + " Приспособленность лучшего " + best.getFitness() );
 	
 						
 	}
 		
 
-	best.writetofile();//нужно записать лучшего в файлж
-	return 1;
-
-	
+	best.writetofile();//нужно записать лучшего в файлж	
 }
 	
 
 static class Helper{
 	  		
 	public static int AVERAGE_ENOUGH=80; 
-	public static int MAX_ENOUGH=90;
-	static int MAX_COUNT=50;	
-	public static int POPULATION_COUNT = 8;
+	public static int MAX_ENOUGH=80;
+	static int MAX_COUNT=4;	
+	public static int POPULATION_COUNT = 6 ;
 	public static double max_fitness = 0;
 	public static double average_fitness = 0;
-	public static float MUTATION_LIKELIHOOD= 5.0F;
-	public static int GENE_MIN = 1; 
-	public static int GENE_MAX = 30;
+	public static float MUTATION_LIKELIHOOD= 20.0F;
+	public static int GENE_MIN = 0; 
+	public static int GENE_MAX = 29;
 	public static int HEAD_SIZE=10;	//размер головы строки 
 	public static int MAX_ARN=4; 		//максимальная арность базовой функции - 4 (у if)
 	public static int GENES_COUNT = 30;
@@ -172,36 +183,36 @@ static class Helper{
 	private static Chromosome popShotEnergy[]=new Chromosome[Helper.POPULATION_COUNT];
 	private static Chromosome popMoveDistance[]=new Chromosome[Helper.POPULATION_COUNT];
 	
-	public int[] getShotAngle() {
-		return popShotAngle;s
+	public static int[] getShotAngle(int i) {
+		return popShotAngle[i].getGenes();
 	}
 	
-	public static void setShotAngle(int[] genes) {
-		popShotAngle = genes;
+	public static void setShotAngle(int i,int[] genes) {
+		popShotAngle[i].setGenes(genes);
 	}
 	
-	public int[] getMoveAngle() {
-		return popMoveAngle;
+	public static int[] getMoveAngle(int i) {
+		return popMoveAngle[i].getGenes();
 	}
 	
-	public static void setMoveAngle(int[] genes) {
-		this.popMoveAngle = genes;
+	public static void setMoveAngle(int i,int[] genes) {
+		popMoveAngle[i].setGenes(genes);
 	}
 	
-	public int[] getShotEnergy() {
-		return popShotEnergy;
+	public static int[] getShotEnergy(int i) {
+		return popShotEnergy[i].getGenes();
 	}
 	
-	public static void setShotEnergy(int[] genes) {
-		this.popShotEnergy = genes;
+	public static void setShotEnergy(int i,int[] genes) {
+		popShotEnergy[i].setGenes(genes);
 	}
 	
-	public int[] getMoveDistance() {
-		return popMoveDistance;
+	public int[] getMoveDistance(int i) {
+		return popMoveDistance[i].getGenes();
 	}
 	
-	public static void setMoveDistance(int[] genes) {
-		this.popMoveDistance = genes;
+	public static void setMoveDistance(int i,int[] genes) {
+		popMoveDistance[i].setGenes(genes);
 	}
 	
 	public static int getRandomInt( int min, int max ){
@@ -246,59 +257,69 @@ static class Helper{
 			for(int j=0;j<POPULATION_COUNT;j++)
 				for(int k=0;k<POPULATION_COUNT;k++)
 					for(int z=0;z<POPULATION_COUNT;z++){
-						specimen.setIndiv(0,Helper.getShotAngle()[i]);
-						specimen.setIndiv(1,Helper.getMoveAngle()[j]);
-						specimen.setIndiv(2,Helper.getShotEnergy()[k]);
-						specimen.setIndiv(3,Helper.getShotEnergy()[z]);
+						specimen.setIndiv(0,Helper.getShotAngle(i));
+						specimen.setIndiv(1,Helper.getMoveAngle(j));
+						specimen.setIndiv(2,Helper.getShotEnergy(k));
+						specimen.setIndiv(3,Helper.getShotEnergy(z));
 						specimen.writetofile();	
 						generate_battle();
-						Runtime.getRuntime().exec("cmd /c robocode.bat");
+						
+							 //Runtime.getRuntime().exec("cmd /c start c:/robocode/robo.bat");
+							//Runtime.getRuntime().exec("cmd /c server.bat", null, new File(" c:/robocode/robo.bat"));
+							runProgramAndWait();
+
 						specimen.scan_result();//узнаем значение приспособленности
 						Hlist.add(specimen);
 					}
 	}
 	
 	
-	static void selection() 		// используем турнирный метод селекции сначала делим все особи на пары по 2 выбираем лучшую, затем пары по три и т.д.
-	{	int k=2;
-		int j=2;
-		while(k<=8)
+	static void selection() 		// используем турнирный метод селекции сначала делим все особи на пары по 5 выбираем лучшую.
+	{	int k=0;
+		int j=POPULATION_COUNT-1;
+		System.out.println("Размер "+ Helper.Hlist.size());
+		while(k<3)
 		{
-			for(int i=0;i<Hlist.size();){	
-				while(j>0){
-					Tmplist.add(Hlist.get(i));
+			for(int i=0;i<Hlist.size();i+=POPULATION_COUNT){	
+				while(j>=0){
+					Tmplist.add(Hlist.get(i+j));
 					j--;
 				}
 				Nlist.add(find_max_fitness(Tmplist));
-				j=k;
-				i+=k;
 				Tmplist.clear();
+				j=POPULATION_COUNT-1;
 			}
 			Hlist.clear();
-			Hlist=Nlist;
+			for(int l=0;l<Nlist.size();l++)
+			Hlist.add(Nlist.get(l));
 			Nlist.clear();
-			k++;
+			++k;
+			System.out.println("Размер "+ Helper.Hlist.size());
 		}
 			
 	}
-	static Specimen find_max_fitness(ArrayList<Specimen> list) ////возвращает особь с самой большой приспособленностью
+	static Specimen find_max_fitness(ArrayList<Specimen> list) //возвращает особь с самой большой приспособленностью
 	{	int max=0;
+		int j=0;
 		for(int i=0;i<list.size();i++)
-			if(list.get(i).fitness>max)
-				max=i;
-		return list.get(max);		
+			if(list.get(i).getFitness()>max){
+				max=list.get(i).fitness;
+				j=i;
+			}
+		return list.get(j);		
 	}
 	
 	public static void destroy(){								//разбирает особи на хромосомы
 		for(int i=0;i<POPULATION_COUNT;i++){
-			setShotAngle(Hlist.get(i).getIndiv(0));
-			setMoveAngle(Hlist.get(i).getIndiv(1));
-			setShotEnergy(Hlist.get(i).getIndiv(2));
-			setMoveDistance(Hlist.get(i).getIndiv(3));
+			setShotAngle(i,Hlist.get(i).getIndiv(0));
+			setMoveAngle(i,Hlist.get(i).getIndiv(1));
+			setShotEnergy(i,Hlist.get(i).getIndiv(2));
+			setMoveDistance(i,Hlist.get(i).getIndiv(3));
 		}
+		Hlist.clear();							//чистим список перед новым созданием особей
 	}	
 	static void crossandmut(){										//скрещиваем хромосомы попарно и мутируем их
-		for(int i=0;i<POPULATION_COUNT;i++){
+		for(int i=0;i<POPULATION_COUNT-1;i++){
 			popShotAngle[i].doubleCrossover(popShotAngle[i+1]);
 			popShotAngle[i].mutation();
 			popShotAngle[i+1].mutation();
@@ -316,19 +337,40 @@ static class Helper{
 	}
 		static int maxfitness(){
 			int max=0;
-			for(int i=0;i<Hlist.size();i++)
-				if((Hlist.get(i).fitness)>max)
-					max=i;
-			return Hlist.get(max).getFitness();
-		}
+			int j=0;
+			for(int i=0;i<Hlist.size();i++){
+				if((Hlist.get(i).getFitness())>max){
+					max=Hlist.get(i).fitness;
+					j=i;
+				}	
+			}	
+			return Hlist.get(j).getFitness();
+			}
 		
-}
+		}	
+	 static boolean runProgramAndWait(){
+		Process process;
+		
+		String[] command = { "cmd", "/C", "Start/Wait", "c:/robocode/robo.bat" };
+		java.lang.Runtime runtime;
 	
+		try{
+			runtime = Runtime.getRuntime();
+			process = runtime.exec(command);
+			process.waitFor();
+			return true;
+		}catch (InterruptedException e){
+			return false;
+		}
+		catch (Exception e){
+         return false;
+		}
+	}
 
 	
 	static class Specimen{										//служит для объединения хромосом в одной особи
 		public int fitness;
-		public int indiv[][]= new int[4][30];
+		public int indiv[][]= new int[4][29];
 		public int getFitness() {
 			return this.fitness;
 		}
@@ -347,14 +389,17 @@ static class Helper{
 			int i, j;
 			BufferedWriter out;
 			try 
-			{
-				out = new BufferedWriter(new FileWriter("chromos.bs"));
+			{	
+				out = new BufferedWriter(new FileWriter("C:\\Users\\Admin.Admin-ПК\\workspace\\Predator\\bin\\EMK\\base\\chromos.bs"));
 				for(i = 0; i < 4; i++){
 					for(j = 0; j < 30; j++){
 						out.write((indiv[i][j])+" " );
 					}
 					out.newLine();
 				}
+				//out.newLine();
+				//out.write(this.fitness);
+				System.out.println("записали особь\n"); 
 				out.close();
 		    }
 		    catch (IOException e) { 
@@ -370,9 +415,9 @@ static class Helper{
 			BufferedReader br;
 			try
 		    {
-				fs= new FileInputStream("result.txt"); 
+				fs= new FileInputStream("C:\\robocode\\results.txt"); 
 				br = new BufferedReader(new InputStreamReader(fs));
-				for(i = 0; i < 2; ++i) 
+				for(i = 0; i < 2;i++) 
 					br.readLine();
 				line = br.readLine();
 		    
@@ -381,26 +426,39 @@ static class Helper{
 				i=line.indexOf('%');
 				if(i!=0){
 					result=line.substring(i-2, i);
+						if(result.indexOf("(")!=-1)							//если приспособленность = 0 нужно считать на символ меньше 
+							result=line.substring(i-1, i);
+					System.out.println("приспособленность " +(Integer.parseInt(result))+ " поколение " + generation);
 					this.setFitness(Integer.parseInt(result));
 				}
-					
+				fs.close();
+				br.close();	
 			}else{
-				line = br.readLine();
-				i=line.indexOf('%');
-				if(i!=0){
-					result=line.substring(i-2, i);
-					this.setFitness(Integer.parseInt(result));
+					line = br.readLine();
+					if(line.indexOf('%')!=-1){
+						i=line.indexOf('%');
+						if(i!=0){
+							result=line.substring(i-2, i);
+						if(result.indexOf("(")!=-1)
+							result=line.substring(i-1, i);
+						if(result.indexOf("00")!=-1)
+							result=line.substring(i-3, i);
+						System.out.println("приспособленность " +(Integer.parseInt(result))+ " поколение " + generation);
+						this.setFitness(Integer.parseInt(result));
+					}else	System.out.println("Непонятно, почему-то здесь ошибочка ");
 				}
+				fs.close();
+				br.close();
 			}
 		    }
-			 
 		    catch(FileNotFoundException e)
 		    {
-		    	System.out.println("Файл не найден");
+		    	System.out.println("Файл result не найден");
 		    }
 			 catch (IOException e) { 
-			    	System.out.println("файл chromos.bs не открывается\n");  
+			    	System.out.println("файл result.txt не открывается\n");  
 			 }
+			
 			
 	}						
 		
--- Predator.java
+++ Predator.java
@@ -1,14 +1,32 @@
+package EMK;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-
 import robocode.*;
+import robocode.util.Utils;
 public class Predator extends AdvancedRobot {
-	
+	private double x, y, ahead, angl, wall, dh, a, GH, vekt, dist, e, E;
+	static class Event{
+	    double x, y, ahead, angl, wall, dh, a, GH, vekt, dist, e, E;
+		Event(double x, double y, double ahead, double angl, double wall, double dh, double a, double GH, double vekt, double dist, double e, double E) {
+			this.x=x;
+			this.y=y;
+			this.ahead=ahead;
+			this.angl=angl;
+			this.wall=wall;
+			this.dh=dh;
+			this.a=a;
+			this.GH=GH;
+			this.vekt=vekt;
+			this.dist=dist;
+			this.e=e;
+			this.E=E;
+		}
+	}
 	
 	
 	static class Tree {
-		  static class Node {
+		static class Node {
 			  int value;
 			  Node left;
 			  Node right;
@@ -17,7 +35,12 @@ public class Predator extends AdvancedRobot {
 		    }
 		  }
 
-		  public void insert(Node node, int value) {	//чем меньше число, тем выше приоритет операции
+		  public static void insert(Node node, int value) {	//чем меньше число, тем выше приоритет операции
+			 if(node == null || node.value == 0)
+			 {
+				 node = new Node(value);
+				 return;
+			 }else
 		    if (value < node.value) {
 		      if (node.left != null) {
 		        insert(node.left, value);
@@ -34,35 +57,109 @@ public class Predator extends AdvancedRobot {
 		    }
 		  }
 
-		  public void assembly_fun(Node node) {
+		  public static double assembly_fun(Node node) {
 		    if (node != null) {
-		    	assembly_fun(node.left);
-		      	assembly_fun(node.right);
-		    }
-		  }
+		      	switch(node.value){
+				case 0:
+					return ((assembly_fun(node.left))*(assembly_fun(node.right)));
+				case 1:
+					return ((assembly_fun(node.left))/(assembly_fun(node.right)));
+				case 2:
+					return ((assembly_fun(node.left))+(assembly_fun(node.right)));
+				case 3:
+					return ((assembly_fun(node.left))+(assembly_fun(node.right)));
+				case 4:
+					return (Math.sqrt(assembly_fun(node.left)));							//корень
+				case 5:
+					return (Math.log(assembly_fun(node.left)));								//натуральный логарифм
+				case 6:
+					return ((assembly_fun(node.left))*(-1));								//значение с минусом
+				case 7:
+					return (Math.min(assembly_fun(node.left),assembly_fun(node.right)));	//минималььное из двух
+				case 8:
+					return (Math.max(assembly_fun(node.left),assembly_fun(node.right)));	//максимальное из двух	
+				case 9:
+					return (Math.atan(assembly_fun(node.left)));							//значение тангенса в радианах
+				case 10:
+					return (Math.exp(assembly_fun(node.left)));
+			}
+			switch(node.value){
+				case 11:
+					return event.x;
+				case 12:
+					return event.y;
+				case 13:
+					return event.wall;
+				case 14:
+					return event.vekt;
+				case 15:
+					return event.e;
+				case 16:
+					return event.dh;
+				case 17:
+					return event.dist;
+				case 18:
+					return event.angl;
+				case 19:
+					return event.GH;
+				case 20:
+					return event.ahead;
+				case 21:
+					return event.E;
+				case 22:
+					return Math.E ;
+				case 23:
+					return Math.PI;
+				case 24:
+					return -0.5 ;
+				case 25:
+					return -1 ;
+				case 26:
+					return 0.5;
+				case 27:
+					return 1;
+				case 28:
+					return 2;
+				case 29:
+					return 0;
+			}	
+								
+		}
+			return 0;
+	}	      	
+		      	
+		      	
+		      	
+		       	
 	}
 	
 	private double ShotAngle; 
     private double ShotEnergy;
     private double MoveAngle;
-    private double MoveDistance; 
+    private double MoveDistance;
+    private Tree.Node tree_sa;
+    private Tree.Node tree_ma;
+    private Tree.Node tree_se;
+    private Tree.Node tree_md;
+    public static	Event event;
     
     public void run() {
-    	static int len=30;
-    	static int[] fun_shotangle= new int[len];
-    	static int[] fun_moveangle = new int[len];
-    	static int[] fun_shotenergy = new int[len];
-    	static int[] fun_movedistance = new int[len];
+    	
+    	int len=30;
+    	int[] fun_shotangle= new int[len];
+    	int[] fun_moveangle = new int[len];
+    	int[] fun_shotenergy = new int[len];
+    	int[] fun_movedistance = new int[len];
 		    try {
-		    	//открываем хромоомы
-		        BufferedReader read = new BufferedReader(new FileReader(getDataFile("chromosome.bs")));
+		    	//открываем хромосомы
+		        BufferedReader read = new BufferedReader(new FileReader("C:\\Users\\Admin.Admin-ПК\\workspace\\Predator\\bin\\EMK\\base\\chromos.bs"));
 
 				//считываем четыре строки
 				String[] str1 = read.readLine().split(" ");
 				String[] str2 = read.readLine().split(" ");
 				String[] str3 = read.readLine().split(" ");
 				String[] str4 = read.readLine().split(" ");
-			
+				System.out.println(str1);
 				for(int i = 0; i < len; i++){						//конвертируем в массив чисел
 					fun_shotangle[i] = Integer.parseInt(str1[i]);
 					fun_moveangle[i] = Integer.parseInt(str2[i]);
@@ -70,26 +167,70 @@ public class Predator extends AdvancedRobot {
 					fun_movedistance[i] = Integer.parseInt(str4[i]);
 				}
 
-		    } catch (IOException exc) {
-		    	
+		    } 
+		    catch (IOException exc) {
+		    	System.out.println("Ошибка считывания");
 		    }
-		    
-		    } catch (NumberFormatException exc) {
+		    catch (NumberFormatException exc) {
 		    	
-				
+		    	System.out.println("Ошибка! Считано не число");
 			}			
-			
-			
-					
-	}
+		    tree_sa=new Tree.Node(fun_shotangle[0]);
+		    tree_ma=new Tree.Node(fun_moveangle[0]);
+		    tree_se=new Tree.Node(fun_shotenergy[0]);
+		    tree_md=new Tree.Node(fun_movedistance[0]);
+		    for(int i = 1; i < len; i++){						//сборка дерева
+		    	Tree.insert(tree_sa,fun_shotangle[i]); 
+		 		Tree.insert(tree_ma,fun_moveangle[i]);
+		 		Tree.insert(tree_se,fun_shotenergy[i]);
+		 		Tree.insert(tree_md,fun_movedistance[i]);
+			}
+		    
 		while(true) {
 			
 
 			setTurnRadarRightRadians(2*Math.PI);
-		
+			ahead = getDistanceRemaining();
+            angl = getTurnRemainingRadians();
+			wall = Wall_dist();
+			GH = getGunHeadingRadians();
+            E = getEnergy();
 
+			event=new Event(x, y, ahead, angl, wall, dh, a, GH, vekt, dist, e, E);
 			
+			ShotAngle = Tree.assembly_fun(tree_sa);
+			ShotEnergy = Tree.assembly_fun(tree_ma);
+			MoveAngle = Tree.assembly_fun(tree_se);
+			MoveDistance = Tree.assembly_fun(tree_md);
+			setTurnGunRightRadians(ShotAngle);
+			if (getGunHeat() == 0 && ShotEnergy > 0) {
+				setFire(ShotEnergy);
+			}
+			setAhead(MoveDistance);
+			setTurnRight(MoveAngle);
+			execute();
 		
         }
     }
-}
+
+
+	
+	private double Wall_dist() {
+        return Math.min(Math.min( getX(),getBattleFieldWidth() - getX()),Math.min(getY(),getBattleFieldHeight() - getY()));
+    }
+	public void onScannedRobot(ScannedRobotEvent event) {	
+				a = Utils.normalRelativeAngle(getHeadingRadians() + event.getBearingRadians());
+				e = event.getEnergy();
+		        dist = event.getDistance();
+		        x = dist * Math.sin(a);
+		        y = dist * Math.cos(a);
+		        vekt = event.getHeadingRadians();
+		       	dh=-Utils.normalRelativeAngle(getGunHeadingRadians()-a);
+
+		    }
+
+		    public void onHitByWall(HitByBulletEvent e) {
+		        turnLeft(180);
+		    }
+	
+}	
